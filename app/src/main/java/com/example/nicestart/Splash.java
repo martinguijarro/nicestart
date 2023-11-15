60dc09e2c47f5aa5b4f28d0c3193ea8e0225ade9
martinguijarro <martin.gg222@gmail.com>
2023-11-15 12:52:03 +0100

    - Added start on Splash - Added application icon - Added cache strategy to Glide on app load - Splash redirect to Login - Deleted default splashscreen

--- app/src/main/java/com/example/nicestart/Splash.java
+++ app/src/main/java/com/example/nicestart/Splash.java
@@ -2,11 +2,14 @@ package com.example.nicestart;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 
 public class Splash extends AppCompatActivity {
@@ -22,8 +25,25 @@ public class Splash extends AppCompatActivity {
                 .load("https://t4.ftcdn.net/jpg/00/53/45/31/360_F_53453175_hVgYVz0WmvOXPd9CNzaUcwcibiGao3CL.jpg")
                 .transition(DrawableTransitionOptions.withCrossFade(2000))
                 .centerCrop()
-                .circleCrop()
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
                 .placeholder(new ColorDrawable(this.getResources().getColor(R.color.teal_200)))
                 .into(mImage);
+
+        openApp();
+    }
+
+    private void openApp() {
+        Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash
+                        .this, Login.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        }, 5000);
     }
 }
