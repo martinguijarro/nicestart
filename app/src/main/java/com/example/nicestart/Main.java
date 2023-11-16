38a051ce4556586c7b0bdf9d0cfd876353c32a9c
martinguijarro <martin.gg222@gmail.com>
2023-11-16 13:31:43 +0100

    - Added Glide to Splash and Main - Added alpha filter to Splash image - Added landscape to Login and Signup - Deleted action bar and logo in Signup

--- app/src/main/java/com/example/nicestart/Main.java
+++ app/src/main/java/com/example/nicestart/Main.java
@@ -3,6 +3,9 @@ package com.example.nicestart;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class Main extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView mWIP = findViewById(R.id.wip);
+
+        Glide.with(this)
+                .load(R.mipmap.ic_wip_foreground)
+                .into(mWIP);
     }
 }
--- app/src/main/java/com/example/nicestart/Signup.java
+++ app/src/main/java/com/example/nicestart/Signup.java
@@ -6,6 +6,9 @@ import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class Signup extends AppCompatActivity {
 
@@ -13,6 +16,12 @@ public class Signup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        ImageView mGirl = findViewById(R.id.girl);
+
+        Glide.with(this)
+            .load(R.drawable.girl)
+            .into(mGirl);
     }
 
     public void openMain(View v) {
@@ -24,6 +33,8 @@ public class Signup extends AppCompatActivity {
 
     public void openLogin(View v) {
         Intent intent = new Intent(Signup.this, Login.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
     }
 }
